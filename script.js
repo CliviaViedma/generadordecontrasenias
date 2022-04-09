d0a2d8939f3a83443f08f084440c502e583c6363
CliviaViedma <cliviaviedma@gmail.com>
2022-04-09 19:35:33 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,92 +1,64 @@
 const body = document.createElement ("body");
+const card = document.createElement("div");
 const input = document.createElement ("input");
+card.classList.add("card");
 input.classList.add("input");
-
+document.body.appendChild(card)
+card.appendChild(input);
 
 const contenedor = document.createElement("div");
 contenedor.classList.add("contenedor");
 const h1 = document.createElement("h1");
 const textH1 = document.createTextNode ("Personalice su contraseña")
+h1.classList.add("titulo");
 h1.appendChild(textH1);
-const form = document.createElement ("form")
-form.classList.add("form");
+card.appendChild(contenedor);
 contenedor.appendChild(h1);
+
+const form = document.createElement ("form")
 contenedor.appendChild(form);
-document.body.appendChild(input);
-document.body.appendChild(contenedor);
+form.classList.add("form");
 
+const unaFuncion = (tipoInput, items, key) => {
 
-const long = [12, 9, 6];
-for(const elem of long) {
-    const contenedor = document.createElement ("div");
-    const label = document.createElement("label")
-        label.setAttribute ("for", "check-${elem}")
-        label.textContent = `${elem} caracteres`
+    const fieldset = document.createElement('fieldset');
 
-    const input = document.createElement('input')
-    input.setAttribute("type", "radio")
-    input.setAttribute("id", `check-${elem}`)
-    input.setAttribute("name", "select-length")
-    input.setAttribute("value", `${elem}`)
-
-    contenedor.appendChild(input)
-    contenedor.appendChild(label)
-    form.appendChild(contenedor)
-
-    input.addEventListener('change', (e) => {
-        generatePassword(e.target.value);
-    })
-   
-}
-
-const reglas = ["Solo letras", "Lectura simple", "Todos los caracteres"]
-const titulo = document.createElement("h3");
-const texth3 = document.createTextNode ("reglas");
-titulo.appendChild(texth3)
-contenedor.appendChild(titulo);
-
-for( const elem of reglas){
-const contenedor = document.createElement ("div");
-    contenedor.classList.add(`contenedorUno`);
-    //const titulo = document.createElement("h3");
-    //contenedor.appendChild(titulo);
+    for(const elem of items) {
+        const label = document.createElement("label")
+        label.setAttribute ("for", `check-${elem}`)
+        label.textContent = `${elem}`
+        label.classList.add("label")
     
-    const label = document.createElement("label");
-        label.setAttribute ("for", "check-${elem}")
-        label.textContent = `${elem} `
-    const input = document.createElement('input')
-        input.setAttribute("type", "radio")
+        const input = document.createElement('input')
+        input.setAttribute("type", tipoInput)       
         input.setAttribute("id", `check-${elem}`)
-        
-input.setAttribute("name", "select-length")
+        input.setAttribute("name", `${key}-control`) 
         input.setAttribute("value", `${elem}`)
+
+        input.addEventListener('change', (e) => {
+            //generatePassword(e.target.value);
+            console.log(e.target.value)
+        })
+
+        const contenedor = document.createElement("div");
         contenedor.appendChild(input)
         contenedor.appendChild(label)
-        form.appendChild(contenedor)
-    
-        input.addEventListener('change', (e) => {
-            generatePassword(e.target.value);
-        })
-       
-    }
-const caracteres = ["Mayúsculas", "Minúsculas", "Numeros", "Simbolos"]
-for(const elem of caracteres){
-const contenedor = document.createElement ("div");
-const label = document.createElement("label")
-            label.setAttribute ("for", "check-${elem}")
-            label.textContent = `${elem} `
-const input = document.createElement('input')
-            input.setAttribute("type", "checkbox")
-            input.setAttribute("id", `check-${elem}`)
 
-            input.setAttribute("name", "select-length")
-                input.setAttribute("value", `${elem}`)
-                contenedor.appendChild(input)
-                contenedor.appendChild(label)
-                form.appendChild(contenedor)
-            
-                input.addEventListener('change', (e) => {
-                    generatePassword(e.target.value);
-                })
-               
-            }  
+        fieldset.appendChild(contenedor)
+     
+    }
+
+    form.appendChild(fieldset);
+}
+
+const long = [12, 9, 6]; //to do
+const reglas = ["Solo letras", "Lectura simple", "Todos los caracteres"] 
+const caracteres = ["Mayúsculas", "Minúsculas", "Números", "Símbolos"]
+unaFuncion('radio', long, 'long')
+unaFuncion('radio', reglas, 'rules')  
+unaFuncion('checkbox', caracteres, 'chars')
+
+// poner la palabra carateres despues de los numeros
+//subtitulo para cADA BLOQUE
+//disabel
+//llamar la funcion generar contraseña en el input
